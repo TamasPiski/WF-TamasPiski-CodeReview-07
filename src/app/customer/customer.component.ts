0b9948348d61058abb4171b51ed99f211080239c
Tamas Piski <tpiski@gmail.com>
2019-11-08 11:22:50 +0100

    getting closer

--- src/app/customer/customer.component.ts
+++ src/app/customer/customer.component.ts
@@ -9,8 +9,20 @@ import { CustomerService  } from "../shared/customer.service";
 export class CustomerComponent implements OnInit {
 
 constructor(private customerService: CustomerService) { }
-
+  submitted: boolean;
+  formControls = this.customerService.form.controls;
+  
   ngOnInit() {
   }
+
+  
+  onSubmit(){
+    // here we said that if the value of the hidden input is null and it's by default null we will insert a new customer and when the $key value is not null we will update an existing data
+            if(this.customerService.form.get("$key").value == null ){
+                    // insert
+            } else {
+                    //update
+            }
+    }
 
 }
--- src/app/shared/customer.service.ts
+++ src/app/shared/customer.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { FormControl , FormGroup } from "@angular/forms";
+import { FormControl , FormGroup , Validators } from "@angular/forms";
 
 @Injectable({
   providedIn: 'root'
@@ -9,9 +9,9 @@ export class CustomerService {
   constructor() { }
   form = new FormGroup({
   $key: new FormControl(null),
-  fullName: new FormControl(''),
-  email: new FormControl(''),
-  mobile: new FormControl(''),
+  fullName: new FormControl('', Validators.required),//We add Validators option and we used required so the user must fill the input
+  email: new FormControl('', Validators.email),// to check if the value inside the input is an email
+  mobile: new FormControl('', [Validators.required, Validators.minLength(8)]), // here we put an array because we want the user to fill the input and the input length must be at least 8
   location: new FormControl('')
-        });
+      });
 }
